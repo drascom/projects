7a69a374919262a92deb8ec414707e96c15e0b14
drascom <drayhancolak@gmail.com>
2025-05-01 23:59:44 +0100

    .

--- setup-repo.sh
+++ setup-repo.sh
@@ -16,7 +16,7 @@ Repository initialized locally. To push to GitHub:
 1. Create a new repository on GitHub (https://github.com/new)
 2. Run the following commands:
 
-git remote add origin https://github.com/YOUR_USERNAME/YOUR_REPO_NAME.git
+git remote add origin https://github.com/drascom/projects.git
 git branch -M main
 git push -u origin main
 "
